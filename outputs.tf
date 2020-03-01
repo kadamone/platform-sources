eebd0e4e0117c624909d31b55b75f8009d344480
Radu Turcu <rturcu@hotmail.com>
2020-03-01 19:38:42 +0200

    added outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "project_id" {
+  value = var.google_project_id
+}
+
+#output "dns_name_servers" {
+#  value = module.nucleus.dns_name_servers
+#}
+
+#output "postgres_instance_name" {
+#  value = module.nucleus.postgres_instance_name
+#}
